2df5e076f838464f2a4944a5154383b51245d2ca
Merk42 <mark_c@markecurtis.com>
2025-05-08 12:00:10 -0400

    better match vote and results layouts

--- client/src/components/modes/Results.tsx
+++ client/src/components/modes/Results.tsx
@@ -28,7 +28,7 @@ function Results({ acros, id }: ResultsProps) {
                 <p className="text-xl">Here are the results</p>
                 <ul className="list-none">
                     { sortedAcro.map((entry:any) => 
-                    <li className={`border-2 p-4 flex gap-2 mb-2 rounded-md ${entry.id === id ? 'border-green-500' : 'border-blue-500'}`} key={entry.id}>
+                    <li className={`border-2 p-4 flex gap-2 mt-4 rounded-md ${entry.id === id ? 'border-green-500' : 'border-blue-500'}`} key={entry.id}>
                         <span className="w-5 text-right font-bold text-blue-500 dark:text-blue-400">{entry.votes}</span>
                         <span className="flex-auto">{entry.acro}</span>
                     </li>
