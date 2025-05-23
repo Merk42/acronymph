fd4f2e8f79d89b018c8fecf061141857fdfb72d2
Merk42 <mark_c@markecurtis.com>
2025-05-23 11:42:52 -0400

    easier to read round display

--- client/src/components/RoundDisplay.tsx
+++ client/src/components/RoundDisplay.tsx
@@ -1,6 +1,15 @@
 function RoundDisplay({ round, category }: { round: number, category: string } ) {
+    const displayCopy = () => {
+        if (round >= 99) {
+            return 'lightning'
+        }
+        if (category) {
+            return `Round ${round}: ${category}`
+        }
+        return `Round ${round}`
+    }
     return (
-        <h1 className="text-2xl font-bold grow">Round {round}{ category ? ': ' + category : ''}</h1>
+        <h1 className="text-2xl font-bold grow">{displayCopy()}</h1>
     )
 }
 
