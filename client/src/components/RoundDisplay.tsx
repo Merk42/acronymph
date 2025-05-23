9f2f2225ea64edc60fd4b596dcb7662ae3b15f0f
Merk42 <mark_c@markecurtis.com>
2025-05-23 15:38:37 -0400

    more verbose round display

--- client/src/components/RoundDisplay.tsx
+++ client/src/components/RoundDisplay.tsx
@@ -1,15 +1,15 @@
 function RoundDisplay({ round, category }: { round: number, category: string } ) {
     const displayCopy = () => {
         if (round >= 99) {
-            return 'lightning'
+            return 'lightning round'
         }
         if (category) {
-            return `Round ${round}: ${category}`
+            return `round ${round}: ${category}`
         }
-        return `Round ${round}`
+        return `round ${round}`
     }
     return (
-        <h1 className="text-2xl font-bold grow">{displayCopy()}</h1>
+        <h1 className="text-2xl font-bold grow capitalize">{displayCopy()}</h1>
     )
 }
 
