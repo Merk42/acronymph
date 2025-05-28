b6b82ddd9c4c1f8c79878ad85d668e2f0318fd38
Merk42 <mark_c@markecurtis.com>
2025-05-28 14:01:11 -0400

    orphan-less instructions

--- client/src/components/Instructions.tsx
+++ client/src/components/Instructions.tsx
@@ -4,11 +4,11 @@ function Instructions() {
         <div className="max-w-5xl m-auto mt-4">
             <h1 className="text-2xl">How to play</h1>
             <ul>
-                <li>All players are given the same random letters (e.g. AIF)</li>
-                <li>Each player submits their entry (e.g. Acronymph Is Fun)</li>
-                <li>Each player then votes on their favorite </li>
-                <li>If there are categories, the winner chooses a category from a random list</li>
-                <li>After 10 rounds, the top two players enter the lightning round, where they submit entries for 3 sets of letters, all other players vote</li>
+                <li className="text-pretty">All players are given the same random letters (e.g. AIF)</li>
+                <li className="text-pretty">Each player submits their entry (e.g. Acronymph Is Fun)</li>
+                <li className="text-pretty">Each player then votes on their favorite </li>
+                <li className="text-pretty">If there are categories, the winner chooses a category from a random list</li>
+                <li className="text-pretty">After 10 rounds, the top two players enter the lightning round, where they submit entries for 3 sets of letters, all other players vote</li>
             </ul>
         </div>
     )
